c67619955cfff526a0778405ac3cb0bdcb3b2f53
vanssales <vanssales@gmail.com>
2026-03-16 19:15:50 -0300

    feat: cria estrutura inicial da classe MiniBanco

---
+++ MiniBanco/MiniBanco.java
@@ -0,0 +1,16 @@
+package MiniBanco;
+
+public class MiniBanco {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("MiniBanco iniciado!");
+
+
+
+    }
+
+
+
+}
